c78c94d1f7f0d1267bb6b3923cdd0cbf7b069be3
Gilson Ramos <gilsrms0@gmail.com>
2026-09-04 12:47:53 -0300

    feat: add repository layer

---
+++ backend/src/main/java/com/delivery/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.delivery.repository;
+
+import com.delivery.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByPhone(String phone);
+}
